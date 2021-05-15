7f8c8ff0191668f0bc58438b19c189a702fbba75
sunakan <sunakn03@gmail.com>
2021-05-15 17:32:01 +0900

    cargo fmt

--- 06kyu-sum-of-digits-digital-root/by-rust/src/lib.rs
+++ 06kyu-sum-of-digits-digital-root/by-rust/src/lib.rs
@@ -2,15 +2,12 @@ fn naruhodo(n: i64) -> i64 {
     if n < 10 {
         n
     } else {
-        naruhodo((n/10) + (n%10))
+        naruhodo((n / 10) + (n % 10))
     }
 }
 
 fn digital_root(n: i64) -> i64 {
-    let sum = n.to_string()
-        .chars()
-        .map(|c| c as i64 - 48)
-        .sum();
+    let sum = n.to_string().chars().map(|c| c as i64 - 48).sum();
     if sum < 10 {
         sum
     } else {
